dd8c1f85654b6935c97640245a32ff3c93b56354
Vasyl Sovyak <soulshockers@gmail.com>
2024-07-14 14:32:21 +0300

    Implement SlugResolveTest

---
+++ tests/Feature/SlugResolveTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SlugResolveTest extends TestCase
+{
+    use WithFaker;
+
+    public function test_slug_can_be_resolved(): void
+    {
+        $user = User::factory()->hasUrls(1)->create([
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+        ]);
+
+        $url = $user->urls->first();
+
+        $response = $this->get(route('slug.resolve', $url->slug));
+
+        $response->assertRedirect($url->url);
+    }
+}
