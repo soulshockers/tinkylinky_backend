1583e467eeddbd72e7975066f381092c03b73fac
Vasyl Sovyak <soulshockers@gmail.com>
2024-07-14 13:39:56 +0300

    Improve Scribe API documentation

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -9,6 +9,8 @@ use App\Services\UrlServiceInterface;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Auth;
+use Knuckles\Scribe\Attributes\Authenticated;
+use Knuckles\Scribe\Attributes\QueryParam;
 use Knuckles\Scribe\Attributes\Response;
 use Knuckles\Scribe\Attributes\ResponseFromApiResource;
 use Symfony\Component\HttpFoundation\Response as ApiResponse;
@@ -19,13 +21,18 @@ class UrlController extends Controller
         private readonly UrlServiceInterface $urlService,
     ) {}
 
+    #[Authenticated]
     #[ResponseFromApiResource(UrlResource::class, Url::class, collection: true, paginate: 15)]
+    #[Response(['message' => 'Unauthenticated.'], ApiResponse::HTTP_UNAUTHORIZED, 'Unauthenticated')]
+    #[QueryParam(name: 'page', type: 'int', example: 1)]
     public function index(): AnonymousResourceCollection
     {
         return UrlResource::collection($this->urlService->paginateUserUrls(Auth::user()));
     }
 
+    #[Authenticated]
     #[ResponseFromApiResource(UrlResource::class, Url::class, status: ApiResponse::HTTP_CREATED)]
+    #[Response(['message' => 'Unauthenticated.'], ApiResponse::HTTP_UNAUTHORIZED, 'Unauthenticated')]
     public function store(StoreUrlRequest $request): JsonResponse
     {
         return UrlResource::make($this->urlService->createUserUrl(Auth::user(), $request->getData()))
@@ -33,13 +40,17 @@ class UrlController extends Controller
             ->setStatusCode(ApiResponse::HTTP_CREATED);
     }
 
+    #[Authenticated]
     #[ResponseFromApiResource(UrlResource::class, Url::class)]
+    #[Response(['message' => 'Unauthenticated.'], ApiResponse::HTTP_UNAUTHORIZED, 'Unauthenticated')]
     public function show(int $id): UrlResource
     {
         return UrlResource::make($this->urlService->findUserUrlById(Auth::user(), $id));
     }
 
+    #[Authenticated]
     #[Response(status: ApiResponse::HTTP_NO_CONTENT)]
+    #[Response(['message' => 'Unauthenticated.'], ApiResponse::HTTP_UNAUTHORIZED, 'Unauthenticated')]
     public function destroy(int $id): JsonResponse
     {
         $this->urlService->deleteUserUrl(Auth::user(), $id);
